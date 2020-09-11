1585c08dfccc18d027cf16b6c86a1bb580230b79
Shahroz Javed <50264214+Shahrozjd@users.noreply.github.com>
2020-09-12 04:15:34 +0500

    added welcome screen and routes to login

--- app/src/main/java/com/example/smartparking/WelcomeScreen.java
+++ app/src/main/java/com/example/smartparking/WelcomeScreen.java
@@ -1,14 +1,42 @@
 package com.example.smartparking;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.cardview.widget.CardView;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.smartparking.renter.LoginActivity_renter;
+import com.example.smartparking.tenant.LoginActivity_tenant;
 
 public class WelcomeScreen extends AppCompatActivity {
 
+    CardView tenantcard;
+    CardView rentarcard;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen);
+        tenantcard = findViewById(R.id.tenantcard);
+        rentarcard = findViewById(R.id.rentercard);
+
+        //TENANT APP ROUTE
+        tenantcard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(WelcomeScreen.this,LoginActivity_tenant.class);
+                startActivity(i);
+            }
+        });
+
+        //RENTER APP ROUTE
+        rentarcard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(WelcomeScreen.this, LoginActivity_renter.class);
+                startActivity(i);
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/smartparking/tenant/SignupActivity_tenant.java
@@ -0,0 +1,16 @@
+package com.example.smartparking.tenant;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.smartparking.R;
+
+public class SignupActivity_tenant extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
