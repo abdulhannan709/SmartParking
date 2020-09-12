ac069e8c50070a95b7f38ca39e144549e0c66432
Shahroz Javed <50264214+Shahrozjd@users.noreply.github.com>
2020-09-12 13:23:17 +0500

    login/signup frontend done

--- app/src/main/java/com/example/smartparking/renter/LoginActivity_renter.java
+++ app/src/main/java/com/example/smartparking/renter/LoginActivity_renter.java
@@ -2,15 +2,34 @@ package com.example.smartparking.renter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextClock;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.smartparking.R;
+import com.example.smartparking.tenant.LoginActivity_tenant;
 
 public class LoginActivity_renter extends AppCompatActivity {
 
+    TextView signupscreen;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        signupscreen = findViewById(R.id.txtsignupscreen);
+        signupscreen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent i = new Intent(LoginActivity_renter.this,SignupActivity_renter.class);
+                startActivity(i);
+
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/smartparking/tenant/LoginActivity_tenant.java
+++ app/src/main/java/com/example/smartparking/tenant/LoginActivity_tenant.java
@@ -2,15 +2,31 @@ package com.example.smartparking.tenant;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.smartparking.R;
 
 public class LoginActivity_tenant extends AppCompatActivity {
 
+    TextView signupscreen;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        signupscreen = findViewById(R.id.txtsignupscreen);
+        signupscreen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent i = new Intent(LoginActivity_tenant.this,SignupActivity_tenant.class);
+                startActivity(i);
+
+            }
+        });
     }
 }
